245403850eeb051dcd98bc6399285a54fc25dddb
unknown <antongogol2017@gmail.com>
2022-11-03 17:01:13 +0200

    bugfix in zombie walking script

--- Trout/Assets/Scripts/ZombieController.cs
+++ Trout/Assets/Scripts/ZombieController.cs
@@ -18,8 +18,19 @@ public class ZombieController : MonoBehaviour
     }
     private void MoveToTarget(){
         agent.SetDestination(target.position);
+        if(Vector3.Distance(target.position, transform.position) <= 0.5)
+        {
+            RotateToTarget();
+        }
+    }
+    private void RotateToTarget()
+    {
+        Vector3 dir = target.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(dir, Vector3.up);
+        transform.rotation = rotation;
     }
     private void GetReferences(){
         agent=GetComponent<NavMeshAgent>();
+
     }
 }
