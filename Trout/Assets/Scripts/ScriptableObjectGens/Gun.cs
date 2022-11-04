ec7a85a9eb75c52b9432c43cac1ba1cc190970d4
Zhenyettta <zhecky2004@gmail.com>
2022-11-04 02:08:48 +0200

    +Trout

---
+++ Trout/Assets/Scripts/ScriptableObjectGens/Gun.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Gun", menuName = "Gun")]
+public class Gun : ScriptableObject
+{
+    public string weapon_name;
+    public float firerate;
+    public float aimSpeed;
+    public GameObject prefab;
+}
---
+++ Trout/Assets/Scripts/Weapon.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    #region Variables
+
+    public Gun[] loadout;
+    public Transform weaponParent;
+    public GameObject bulletholePrefab;
+    public LayerMask canBeShot;
+
+    private int currentIndex;
+    private GameObject currentWeapon;
+    #endregion
+
+    #region MonoBehaviour Callbacks
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) Equip(0);
+        if(currentWeapon != null)
+        {
+            Aim(Input.GetMouseButton(1));
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Shoot();
+            }
+        }
+    }
+    #endregion
+
+    #region Private Methods
+
+    void Equip (int p_ind)
+    {
+        if (currentWeapon != null) Destroy(currentWeapon);
+
+        currentIndex = p_ind;
+
+        GameObject t_newWeapon = Instantiate(loadout[p_ind].prefab, weaponParent.position, weaponParent.rotation, weaponParent) as GameObject;
+        t_newWeapon.transform.localPosition = Vector3.zero;
+        t_newWeapon.transform.localEulerAngles = Vector3.zero;
+
+        currentWeapon = t_newWeapon;
+    }
+
+    void Aim(bool p_isAiming)
+    {
+        Transform t_anchor = currentWeapon.transform.Find("Anchor");
+        Transform t_state_ads = currentWeapon.transform.Find("States/ADS");
+        Transform t_state_hip = currentWeapon.transform.Find("States/Hip");
+        
+        if (p_isAiming)
+        {
+            t_anchor.position = Vector3.Lerp(t_anchor.position, t_state_ads.position, Time.deltaTime * loadout[currentIndex].aimSpeed);
+
+        } 
+        else
+        {
+            t_anchor.position = Vector3.Lerp(t_anchor.position, t_state_hip.position, Time.deltaTime * loadout[currentIndex].aimSpeed);
+
+        }
+    }
+
+    void Shoot()
+    {
+        Transform t_spawn = transform.Find("Cameras/Camera");
+
+        RaycastHit t_hit = new RaycastHit();
+        if(Physics.Raycast(t_spawn.position, t_spawn.forward, out t_hit, 1000f, canBeShot))
+        {
+            GameObject t_newHole = Instantiate(bulletholePrefab, t_hit.point + t_hit.normal * 0.001f, Quaternion.identity) as GameObject;
+            t_newHole.transform.LookAt(t_hit.point + t_hit.normal);
+        }
+    }
+
+    #endregion
+}
