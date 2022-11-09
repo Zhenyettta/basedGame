44c7bfbc9aab8db0b88c9452c3b1887efe1a5dd7
Zhenyettta <zhecky2004@gmail.com>
2022-11-09 19:21:28 +0200

    +vityok

--- Trout/Assets/Scripts/PlayerController.cs
+++ Trout/Assets/Scripts/PlayerController.cs
@@ -83,6 +83,11 @@ public class PlayerController : MonoBehaviour
             }
         }
 
+  
+
+
+
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (ground == true && readyToJump)
