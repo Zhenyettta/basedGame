a1f70371d72b199fe7fe37cb05acfafe6adfe19a
VictorSoviak <inode16@gmail.com>
2022-11-10 03:07:09 +0200

    ~

--- Trout/Assets/Scripts/Zombie/ZombieController.cs
+++ Trout/Assets/Scripts/Zombie/ZombieController.cs
@@ -11,7 +11,7 @@ public class ZombieController : MonoBehaviour
 
     public int max_health;
     private int current_health;
-    public int damage;
+    public int zombie_damage = 20;
 
 
     PlayerController pcr = new PlayerController();
@@ -61,22 +61,12 @@ public class ZombieController : MonoBehaviour
     }
 
 
-    public float attackRate = 2f;
-    float nextAttackTime = 0f;
 
     public void DealDamage()
     {
         if (Vector3.Distance(target.position, transform.position) <= 2)
-        {
-            if (attackRate >= nextAttackTime)
-            {
-                pcr.TakeDamageFromEnemy(20);
-                nextAttackTime = Time.deltaTime * 5f;
-            }
-            else
-            {
-                nextAttackTime -= Time.deltaTime; /* if nextAttackTime is greater than the attackRate, subtract one from nextAttackTime. this only happens once per second because you use Time.deltaTime */
-            }
+        { 
+             pcr.TakeDamageFromEnemy(zombie_damage);
 
         }
     }
